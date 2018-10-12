aa33305d9e829e4c60a2107ab7cc693f76be5888
Akafuma <akafuma.af@gmail.com>
2018-10-12 15:50:48 +0200

    Add files via upload

--- src/Radio.java
+++ src/Radio.java
@@ -1,16 +1,17 @@
+package org.xcsp.modeler.problems;
 
 import java.util.ArrayList;
 
 import org.xcsp.common.IVar.Var;
 import org.xcsp.modeler.api.ProblemAPI;
-import org.xcsp.modeler.implementation.NotData;
 
-
-public class Radio implements ProblemAPI
-{
-    int[] regions;
-
+public class Radio implements ProblemAPI {
+	
+	int[] regions;
     Station[] stations;
+    Interference[] interferences;
+    Liaison[] liaisons;
+    
     class Station
     {
         int num;
@@ -19,8 +20,7 @@ public class Radio implements ProblemAPI
         int[] emetteur;
         int[] recepteur;
     }
-
-    Interference[] interferences;
+    
     class Interference
     {
         int x;
@@ -28,39 +28,31 @@ public class Radio implements ProblemAPI
         int Delta;
     }
 
-    Liaison[] liaisons;
     class Liaison
     {
         int x;
         int y;
     }
 
-    @NotData
-    int n_stations;
-    @NotData
-    int n_interfer;
-    @NotData
-    int n_liaisons;
-
     @Override
     public void model()
     {
-        n_stations = stations.length;
-        n_interfer = interferences.length;
-        n_liaisons = liaisons.length;
+        int n_stations = stations.length;
+        int n_interfer = interferences.length;
+        int n_liaisons = liaisons.length;
 
-        // Variables obligatoire au problème et domaine
+        // Variables du problème
         Var[] eme = array("eme", size(stations.length), i -> dom(stations[i].emetteur),
                 "eme[i]: frequence d'emission de la station i");
         Var[] rec = array("rec", size(stations.length), i -> dom(stations[i].recepteur),
                 "rec[i]: frequence de reception de la station i");
 
-
+        
         // Contraintes du problème de base
-        // 1_ ecart de frequence entre eme et rec d'une station.
+        // 1. Ecart de frequence entre eme et rec d'une station.
         forall(range(n_stations), i -> equal(dist(eme[i], rec[i]), stations[i].delta));
         
-        // 2_ ecart de frequence necessaire entre stations proches
+        // 2. Ecart de frequence necessaire entre stations proches
         for(int i = 0; i < n_interfer; i++)
         {
             int x = interferences[i].x;
@@ -73,18 +65,21 @@ public class Radio implements ProblemAPI
             greaterEqual(dist(rec[x], rec[y]),D);  // | rec[x] - rec[y] |  >=  D
         }
         
-        // 3_ liaisons entre stations
+        // 3. Liaisons entre stations
         for(int i = 0; i < n_liaisons; i++)
         {
             equal(eme[liaisons[i].x], rec[liaisons[i].y]);  // eme[i] == rec[j]
             equal(rec[liaisons[i].x], eme[liaisons[i].y]);  // rec[i] == eme[j]
         }
         
-        // 4_ nombre de frequences par regions
+        // 4. Nombre de frequences par regions
+        
+        //Initialisation du tableau pour sauvegarder les variables par région
         ArrayList< ArrayList<Var>> frequenciesByRegions = new ArrayList<ArrayList<Var>>();      
         for(int i = 0; i < regions.length; i++)
         	frequenciesByRegions.add(new ArrayList<Var>());
         
+        //La station i est dans la région r : on ajoute eme[i] et rec[i] dans les fréquences de r
         for(int i = 0; i < n_stations; i++)
         {
         	int r = stations[i].region;
@@ -94,36 +89,27 @@ public class Radio implements ProblemAPI
         
         for(int i = 0; i < regions.length; i++)
         {
-        	Var[] frequencies_region_i = (Var[]) frequenciesByRegions.get(i).toArray();
+        	ArrayList<Var> arr = frequenciesByRegions.get(i);
+        	Var[] frequencies_region_i = arr.toArray(new Var[arr.size()]);
         	nValues(frequencies_region_i, LE, regions[i]);
-        }
-
-        //minimiser le nombre de frequences utilisees
-        if(modelVariant("m1"))
-        {
-        	Var[] allVar = vars(eme, rec); //On récupère toutes les fréquences utilisées
-        	int sum = 0;
-        	for(int i = 0; i < regions.length; i++)
-        		sum+=regions[i];
-        	/*Le nombre maximum de fréquence utilisable est majoré par la somme
-        	 *  du nombre de fréquence
-        	 *  utilisable par chaque régions
-        	 */
+        }        
+        
+        //Minimiser le nombre de frequences utilisees
+     	
+        Var[] allVar = vars(eme, rec); //On récupère toutes les fréquences utilisées
+       	int sum = 0;
+       	for(int i = 0; i < regions.length; i++)
+       		sum+=regions[i];
+       	/*Le nombre maximum de fréquence utilisable est majoré par la somme
+       	 *  du nombre de fréquence
+       	 *  utilisable par chaque régions
+       	 */
         		
-        	Var min = var("min", dom(range(sum)), 
-        			"min est le nombre de fréquence différentes utilisées");
-        	nValues(allVar, EQ, min);
-        	minimize(min);
-        }
-        //utiliser les frequences les plus basses possibles,
-        if(modelVariant("m2"))
-        {
-
-        }
-        //minimiser la largeur de la bande de frequences utilisées
-        if(modelVariant("m2"))
-        {
-
-        }
+       	Var min = var("min", dom(range(sum)), 
+       		"min est le nombre de fréquence différentes utilisées");
+       	//On compte le nombre de fréquence utilisé grâceà la contrainte nvalues et la variable min
+        nValues(allVar, EQ, min);
+        //On minimise donc sur la variable min
+       	minimize(min);
     }
 }
